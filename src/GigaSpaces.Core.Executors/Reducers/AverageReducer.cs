6a99a37900948df5961a5e4ec1919ef46ee5390e
Skyler Severns <skyler@gigaspaces.com>
2014-05-29 17:50:05 -0500

    Adding tests for the average reducer.

--- src/GigaSpaces.Core.Executors/Reducers/AverageReducer.cs
+++ src/GigaSpaces.Core.Executors/Reducers/AverageReducer.cs
@@ -1,6 +1,30 @@
-﻿namespace GigaSpaces.Core.Executors.Reducers
+﻿using System;
+using System.Linq.Expressions;
+using GigaSpaces.Core.Executors.Tasks;
+
+namespace GigaSpaces.Core.Executors.Reducers
 {
-    public class AverageReducer
+    public class AverageReducer<T, T1> : AverageTask<T, T1>, IDistributedSpaceTask<long>
     {
+        public AverageReducer(Expression<Func<T, T1>> averagePropertyExpression) : base(averagePropertyExpression)
+        {
+        }
+
+        public new long Execute(ISpaceProxy spaceProxy, ITransaction tx)
+        {
+            return base.Execute(spaceProxy, tx);
+        }
+
+        public long Reduce(SpaceTaskResultsCollection<long> results)
+        {
+            long sum = 0;
+
+            foreach (var spaceTaskResult in results)
+            {
+                sum += spaceTaskResult.Result;
+            }
+
+            return sum/results.Count;
+        }
     }
 }
