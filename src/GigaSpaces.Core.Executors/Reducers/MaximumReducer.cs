7742013a0097a030df56a3b171f07eb333460475
Skyler Severns <skyler@gigaspaces.com>
2014-05-30 00:08:37 -0500

    Implementing the maximum reducer.

--- src/GigaSpaces.Core.Executors/Reducers/MaximumReducer.cs
+++ src/GigaSpaces.Core.Executors/Reducers/MaximumReducer.cs
@@ -1,7 +1,29 @@
-﻿namespace GigaSpaces.Core.Executors.Reducers
+﻿using System;
+using System.Linq.Expressions;
+using GigaSpaces.Core.Executors.Tasks;
+
+namespace GigaSpaces.Core.Executors.Reducers
 {
-    public class MaximumReducer
+    [Serializable]
+    public class MaximumReducer<T, T1> : MaximumTask<T, T1>, IDistributedSpaceTask<long,long>
     {
-        
+        public MaximumReducer(Expression<Func<T, T1>> maximumPropertyExpression) : base(maximumPropertyExpression)
+        {
+        }
+
+        public long Reduce(SpaceTaskResultsCollection<long> results)
+        {
+            long output = 0;
+
+            foreach (var spaceTaskResult in results)
+            {
+                if (output < spaceTaskResult.Result)
+                {
+                    output = spaceTaskResult.Result;
+                }
+            }
+
+            return output;
+        }
     }
 }
